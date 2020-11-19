ae543c7da39f7cf459d3ae18e3b8ce77c09c100e
Tomoko-tech <tomoko.takahashi715@gmail.com>
2020-11-19 10:04:00 +0900

    ①管理者Aがリポジトリを作成しました

---
+++ NoForkApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NoForkApp
+//
+//  Created by Takahashi Tomoko on 2020/11/19.
+//  Copyright © 2020 takatomo.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("①管理者Aがリポジトリを作成しました")
+    }
+
+
+}
+
